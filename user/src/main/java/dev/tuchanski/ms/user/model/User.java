fb11795a2a4720135f87bece080e6ca0ee113314
Tuchanski <gui.rocha2004@gmail.com>
2025-12-04 15:44:01 -0300

    feat: add user model

---
+++ user/src/main/java/dev/tuchanski/ms/user/model/User.java
@@ -0,0 +1,26 @@
+package dev.tuchanski.ms.user.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
