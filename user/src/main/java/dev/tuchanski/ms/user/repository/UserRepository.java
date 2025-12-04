9f8b18b3402824e3c06b52e5406e8837c0f299cb
Tuchanski <gui.rocha2004@gmail.com>
2025-12-04 15:45:36 -0300

    feat: add user repository

---
+++ user/src/main/java/dev/tuchanski/ms/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.tuchanski.ms.user.repository;
+
+import dev.tuchanski.ms.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(String email);
+}
