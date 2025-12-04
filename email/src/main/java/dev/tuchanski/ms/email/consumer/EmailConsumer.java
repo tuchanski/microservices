1acfad7968c57c490443cb3c8a794cc0df53dc7c
Tuchanski <gui.rocha2004@gmail.com>
2025-12-04 17:14:39 -0300

    feat: inject email service to email consumer

--- email/src/main/java/dev/tuchanski/ms/email/consumer/EmailConsumer.java
+++ email/src/main/java/dev/tuchanski/ms/email/consumer/EmailConsumer.java
@@ -1,16 +1,21 @@
 package dev.tuchanski.ms.email.consumer;
 
 import dev.tuchanski.ms.email.dto.EmailDTO;
+import dev.tuchanski.ms.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+    private final EmailService emailService;
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailDTO dto) {
-        System.out.println(dto.emailTo());
+        emailService.sendEmail(dto);
     }
 
 }
