36dfea1cc95a9774d5037613605d741480f6d4ff
Tuchanski <gui.rocha2004@gmail.com>
2025-12-04 16:36:56 -0300

    feat: inject user producer to user service to publish messages

--- user/src/main/java/dev/tuchanski/ms/user/service/impl/UserServiceImpl.java
+++ user/src/main/java/dev/tuchanski/ms/user/service/impl/UserServiceImpl.java
@@ -4,8 +4,10 @@ import dev.tuchanski.ms.user.dto.UserRequestDTO;
 import dev.tuchanski.ms.user.dto.UserResponseDTO;
 import dev.tuchanski.ms.user.mapper.UserMapper;
 import dev.tuchanski.ms.user.model.User;
+import dev.tuchanski.ms.user.producer.UserProducer;
 import dev.tuchanski.ms.user.repository.UserRepository;
 import dev.tuchanski.ms.user.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +17,10 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final UserProducer userProducer;
 
     @Override
+    @Transactional
     public UserResponseDTO create(UserRequestDTO userRequestDTO) {
         if (userRepository.existsByEmail(userRequestDTO.email())) {
             throw new IllegalArgumentException("E-mail already registered");
@@ -24,6 +28,7 @@ public class UserServiceImpl implements UserService {
 
         User user = userMapper.toEntity(userRequestDTO);
         user = userRepository.save(user);
+        userProducer.publishMessageEmail(user);
 
         return userMapper.toDto(user);
     }
